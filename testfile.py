8731dfc50c3708c353273e28fbdfad96f353e00b
Andreas <andreas409@gmail.com>
2016-05-03 08:05:16 +0200

    added proper document names

--- testfile.py
+++ testfile.py
@@ -15,7 +15,7 @@ def countWords(query):
         return True
 
 
-def runQuery(topic, maxresults, target):
+def runQuery(topic, maxresults, target, extcnt):
     returned = wiki.search(topic, results=maxresults, suggestion=False)
     print "Looking for topics in " + topic
     count = 0
@@ -38,12 +38,15 @@ def runQuery(topic, maxresults, target):
 
         query = result.content.encode('utf-8')
         if countWords(query):
-            f = open(topic + "/output_" + str(count) + ".txt", "w")
+            #f = open(topic + "/output_" + str(count) + ".txt", "w")
+            f = open(topic + "/document" + "." + str(extcnt), "w")
             f.write(result.title.encode('utf-8') + "\n\n\n")
             f.write(result.content.encode('utf-8'))
             count += 1
-            print "Found file " + str(count+1) + "/" + str(target)
+            extcnt += 1
+            print "Found file " + str(count) + "/" + str(target)
             f.close()
+    return extcnt
 
 
 
@@ -57,17 +60,17 @@ topic7 = "Parallel Computing"
 topic8 = "Human Computer Interaction"
 topic9 = "Mobile Application Development"
 topic10 = "Cloud Computing"
-
-runQuery(topic1, 1000, 20)
-runQuery(topic2, 1000, 20)
-runQuery(topic3, 1000, 20)
-runQuery(topic4, 1000, 20)
-runQuery(topic5, 1000, 20)
-runQuery(topic6, 1000, 20)
-runQuery(topic7, 1000, 20)
-runQuery(topic8, 1000, 20)
-runQuery(topic9, 1000, 20)
-runQuery(topic10, 1000, 20)
+extCount = 0
+extCount = runQuery(topic1, 1000, 20, extCount)
+extCount = runQuery(topic2, 1000, 20, extCount)
+extCount = runQuery(topic3, 1000, 20, extCount)
+extCount = runQuery(topic4, 1000, 20, extCount)
+extCount = runQuery(topic5, 1000, 20, extCount)
+extCount = runQuery(topic6, 1000, 20, extCount)
+extCount = runQuery(topic7, 1000, 20, extCount)
+extCount = runQuery(topic8, 1000, 20, extCount)
+extCount = runQuery(topic9, 1000, 20, extCount)
+extCount = runQuery(topic10, 1000, 20, extCount)
 
 
 print "Done Processing"
